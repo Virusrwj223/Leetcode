ecbccd417ea7c4408230f9ecfa89cdddd76333ef
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-23 12:14:28 +0800

    2054. Two Best Non-Overlapping Events

--- greedy/2054. Two Best Non-Overlapping Events.py
+++ greedy/2054. Two Best Non-Overlapping Events.py
@@ -1,4 +1,16 @@
 '''
+The question is:
+You are given a 0-indexed 2D integer array of events where events[i] = [startTimei, endTimei, valuei]. The ith event starts at startTimei and ends at endTimei, and if you attend this event, you will receive a value of valuei. You can choose at most two non-overlapping events to attend such that the sum of their values is maximized.
+
+Return this maximum sum.
+
+Note that the start time and end time is inclusive: that is, you cannot attend two events where one of them starts and the other ends at the same time. More specifically, if you attend an event with end time t, the next event must start at or after t + 1.
+
+The point of this question is to efficiently find the maximal non-overlapping values.
+
+A naive implementation involves a nested loop searching for the next best value.
+This can be imporved through sorting and binary searching
+
 Errors made: Finding the best sequence of non-overlapping events that maximise value instead of the best of 2.
 
 '''
