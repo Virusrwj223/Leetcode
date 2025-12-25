e1b51b0f148745409e5d38294d61969886c09c23
Virusrwj223 <mhrishiraj@gmail.com>
2025-12-25 12:15:54 +0800

    3075. Maximize Happiness of Selected Children

--- greedy/3075. Maximize Happiness of Selected Children.py
+++ greedy/3075. Maximize Happiness of Selected Children.py
@@ -16,7 +16,7 @@ def main():
     print(1, candidate([1], 1) == 1)
     print(2, candidate([1, 2], 1) == 2)
     print(3, candidate([1, 2], 2) == 2)
-    print(4, candidate([2, 1], 1) == 1)
+    print(4, candidate([2, 1], 1) == 2)
     print(5, candidate([2, 1, 1], 3) == 2)
     print(6, candidate([1, 2, 3], 2) == 4)
     print(7, candidate([1, 1, 1, 1], 2) == 1)
